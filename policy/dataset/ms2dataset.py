Fixing masking bugs

policy/dataset/ms2dataset.py
@@ -294,101 +294,59 @@ def get_skill_pad_from_seq_pad(seq_pad, max_skill_len):
     return skill_pad_mask
     
 
-# def get_dec_ar_masks(num_img_feats, max_skill_len):
-#     """ 
-#     The masks for this information will be diagonal such that at each timestep, 
-#     the model can only attend to the conditional info at that timestep. This is
-#     for the specific order that is used in the SkillVAE model
-#     """
-#     dec_src_len = max_skill_len * (2 + num_img_feats) # (MSL*(q + img_feats + z))
-#     tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(max_skill_len) #BUG
-#     # tgt_mask = ~(torch.eye(max_skill_len).to(torch.bool)) # Only allow self attention for single step prediction
-#     mem_mask = torch.ones(max_skill_len, dec_src_len).to(torch.bool) # Start with everything masked
-#     src_mask = torch.ones(dec_src_len, dec_src_len).to(torch.bool) # Start with everything masked
-    
-#     sa_mask = ~torch.diag(torch.ones(max_skill_len)).to(torch.bool) # Self attention mask
-#     src_mask[:max_skill_len,:max_skill_len] =  sa_mask # Unmask qpos self attention
-#     src_mask[-max_skill_len:,-max_skill_len:] = sa_mask # Unmask z self attention
-#     for s in range(max_skill_len):
-#         im_start = max_skill_len + s*num_img_feats
-#         im_end = im_start + num_img_feats
-#         zs = -max_skill_len + s
-#         # Decoder mask
-#         src_mask[im_start:im_end, im_start:im_end] = False # Unmask img features self attention block
-#         src_mask[s,im_start:im_end] = False # Unmask qpos attention to img features
-#         src_mask[zs,im_start:im_end] = False # Unmask z attention to img features
-#         src_mask[im_start:im_end,s] = False # Unmask img features attention to qpos
-#         src_mask[im_start:im_end,zs] = False # Unmask img features attention to z
-#         src_mask[s,zs] = False # Unmask qpos attention to z
-#         src_mask[zs,s] = False # Unmask z attention to qpos
-#         # Memory mask, leave column 0 all True to mask attention to start token 
-#         mem_mask[s,im_start:im_end] = False # Unmask action attention to img features
-#         mem_mask[s,zs] = False # Unmask action attention to z
-#         mem_mask[s,s] = False # Unmask action attention to qpos
-
-#     return src_mask, mem_mask, tgt_mask
-
-
 def get_dec_ar_masks(num_img_feats, max_skill_len):
     """ 
     The masks for this information will be diagonal such that at each timestep, 
     the model can only attend to the conditional info at that timestep. This is
     for the specific order that is used in the SkillVAE model
     """
-    dec_src_len = max_skill_len * 2 # (MSL*(q + img_feats + z))
-    # tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(max_skill_len) #BUG
-    tgt_mask = ~(torch.eye(max_skill_len).to(torch.bool)) # Only allow self attention for single step prediction
+    dec_src_len = max_skill_len * (1 + num_img_feats) + 1 # (MSL*(q + img_feats) + z)
+    tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(max_skill_len) #BUG
+    # tgt_mask = ~(torch.eye(max_skill_len).to(torch.bool)) # Only allow self attention for single step prediction
     mem_mask = torch.ones(max_skill_len, dec_src_len).to(torch.bool) # Start with everything masked
     src_mask = torch.ones(dec_src_len, dec_src_len).to(torch.bool) # Start with everything masked
     
-    sa_mask = ~torch.diag(torch.ones(max_skill_len)).to(torch.bool) # Self attention mask
-    src_mask[:max_skill_len,:max_skill_len] =  sa_mask # Unmask qpos self attention
-    src_mask[-max_skill_len:,-max_skill_len:] = sa_mask # Unmask z self attention
+    src_mask[:max_skill_len,:max_skill_len] =  ~torch.diag(torch.ones(max_skill_len)).to(torch.bool) # Unmask qpos self attention
+    src_mask[-1:,-1:] = False # Unmask z self attention
     for s in range(max_skill_len):
+        im_start = max_skill_len + s*num_img_feats
+        im_end = im_start + num_img_feats
         zs = -max_skill_len + s
         # Decoder mask
-        src_mask[s,zs] = False # Unmask qpos attention to z
-        src_mask[zs,s] = False # Unmask z attention to qpos
+        src_mask[im_start:im_end, im_start:im_end] = False # Unmask img features self attention block
+        src_mask[s,im_start:im_end] = False # Unmask qpos attention to img features
+        src_mask[im_start:im_end,s] = False # Unmask img features attention to qpos
+        src_mask[im_start:im_end,-1] = False # Unmask img features attention to z
+        src_mask[s,-1] = False # Unmask qpos attention to z
         # Memory mask, leave column 0 all True to mask attention to start token 
-        mem_mask[s,zs] = False # Unmask action attention to z
+        mem_mask[s,im_start:im_end] = False # Unmask action attention to img features
+        mem_mask[s,-1] = False # Unmask action attention to z
         mem_mask[s,s] = False # Unmask action attention to qpos
 
     return src_mask, mem_mask, tgt_mask
 
 
-# def get_plan_ar_masks(num_img_feats, max_num_skills):
+# def get_dec_ar_masks(num_img_feats, max_skill_len): #BUG
+#     """ 
+#     The masks for this information will be diagonal such that at each timestep, 
+#     the model can only attend to the conditional info at that timestep. This is
+#     for the specific order that is used in the SkillVAE model
 #     """
-#     The masks for this information will be causal for the skills
-#     """
-#     tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(max_num_skills)
-#     # tgt_mask = ~(torch.eye(max_num_skills).to(torch.bool)) # Only allow self attention for single step prediction
-#     # Only pass in the relevant timestep info (at the beginning of each skill)
-#     plan_src_len = max_num_skills * (1 + 2*num_img_feats) # (MNS*(q + img_feats + goal))
-#     mem_mask = torch.ones(max_num_skills, plan_src_len).to(torch.bool) # Start with everything masked
-#     src_mask = torch.ones(plan_src_len, plan_src_len).to(torch.bool) # Start with everything masked
-    
-#     for s in range(max_num_skills):
-#         im_start = max_num_skills + s*num_img_feats
-#         im_end = im_start + num_img_feats
-#         goal_start = max_num_skills*(1 + num_img_feats) + s*num_img_feats
-#         goal_end = goal_start + num_img_feats
-
-#         # Src mask
-#         src_mask[s,s] = False # Unmask qpos self attention
-#         src_mask[im_start:im_end, im_start:im_end] = False # Unmask img features self attention block
-#         src_mask[s,im_start:im_end] = False # Unmask qpos attention to img features
-#         src_mask[goal_start:goal_end,im_start:im_end] = False # Unmask goal attention to img features
-#         src_mask[im_start:im_end,s] = False # Unmask img features attention to qpos
-#         src_mask[im_start:im_end,goal_start:goal_end] = False # Unmask img features attention to goal
-#         src_mask[s,goal_start:goal_end] = False # Unmask qpos attention to goal
-#         src_mask[goal_start:goal_end,s] = False # Unmask goal attention to qpos
-#         src_mask[goal_start:goal_end,goal_start:goal_end] = False # Unmask goal self attention blocks
-#         # Memory mask
-#         mem_mask[s,im_start:im_end] = False # Unmask skill attention to img features
-#         mem_mask[s,goal_start:goal_end] = False # Unmask skill attention to goal
-#         mem_mask[s,s] = False # Unmask skill attention to qpos
+#     dec_src_len = max_skill_len + 1 # (MSL*q + z)
+#     tgt_mask = ~(torch.eye(max_skill_len).to(torch.bool)) # Only allow self attention for single step prediction
+#     mem_mask = torch.ones(max_skill_len, dec_src_len).to(torch.bool) # Start with everything masked
+#     src_mask = torch.ones(dec_src_len, dec_src_len).to(torch.bool) # Start with everything masked    
+#     src_mask[:max_skill_len,:max_skill_len] =  ~torch.diag(torch.ones(max_skill_len)).to(torch.bool) # Unmask qpos self attention
+#     src_mask[-1:,-1:] = False # Unmask z self attention
+#     for s in range(max_skill_len):
+#         # Decoder mask
+#         src_mask[s,-1] = False # Unmask qpos attention to z
+#         # Memory mask, leave column 0 all True to mask attention to start token 
+#         mem_mask[s,-1] = False # Unmask action attention to z
+#         mem_mask[s,s] = False # Unmask action attention to qpos
 
 #     return src_mask, mem_mask, tgt_mask
+
 
 def get_plan_ar_masks(num_img_feats, max_num_skills): #BUG
     """

policy/planning/skill_plan.py
@@ -248,7 +248,7 @@ class TSkillPlan(nn.Module):
         # tgt_pe = self.get_pos_table(tgt.shape[0]).permute(1,0,2) * self.tgt_pos_scale_factor # (MNS|<, 1, hidden_dim)
         # tgt_pe = tgt_pe.repeat(1, bs, 1)  # (MNS|<, bs, hidden_dim)
         tgt_pe = torch.zeros_like(tgt, device=self._device) #BUG
-        tgt_pad_mask = None #BUG With isolated time steps, padding a skill leads to NaNs
+        tgt_pad_mask = None # BUG With isolated time steps, padding a skill leads to NaNs
         tgt = tgt + tgt_pe
         tgt = self.tgt_norm(tgt)
 

policy/skill/skill_vae.py
@@ -296,10 +296,10 @@ class TSkillCVAE(nn.Module):
         Decode an individual skill into actions, possibly based on current 
         image and state depending on self.conditional_decode
         args: 
-            z: (MSL|1, bs, latent_dim)
+            z: (1, bs, latent_dim)
             qpos: (MSL|1, bs, state_dim)
             img_info: (img_src, img_pe): (MSL|1, bs, num_cam, h*w, c & hidden)
-            src_pad_mask: (bs, MSL|1)
+            src_pad_mask: (bs, 1)
             tgt_pad_mask: (bs, MSL|<)
             src_mask: (bs, MSL|<*(2+num_features*num_cam), ")
             mem_mask: (bs, MSL|<, MSL|<*(2+num_features*num_cam))
@@ -332,8 +332,8 @@ class TSkillCVAE(nn.Module):
         dec_type_embed = dec_type_embed.unsqueeze(1).repeat(1, bs, 1) # (4, bs, hidden_dim)
 
         # skills
-        z_src = self.dec_z(z) # (1|MSL, bs, hidden_dim)
-        z_src = self.dec_input_z_norm(z_src) # (1|MSL, bs, hidden_dim)
+        z_src = self.dec_z(z) # (1, bs, hidden_dim)
+        z_src = self.dec_input_z_norm(z_src) # (1, bs, hidden_dim)
         z_src = z_src + dec_type_embed[3, :, :] # add type 4 embedding
         z_pe = torch.zeros_like(z_src) # no pe, only one skill per decoding step
 
@@ -359,31 +359,28 @@ class TSkillCVAE(nn.Module):
             state_src = state_src + dec_type_embed[1, :, :] # add type 2 embedding
             state_pe = torch.zeros_like(state_src) # no pe, only one obs per decoding step
 
-            # # image, only use one image to decode rest of the skills # BUG
-            # img_src, img_pe = img_info # (1|MSL, bs, num_cam, h*w, c&hidden)
-            # img_src = self.image_proj(img_src) # (1|MSL, bs, num_cam, h*w, hidden)
-            # img_src = self.image_feat_norm(img_src)
-            # img_pe = img_pe * self.img_pe_scale_factor
-            # img_src = img_src.flatten(2,3) # (1|MSL, bs, num_cam*h*w, hidden)
-            # img_pe = img_pe.flatten(2,3)
-            # img_src = img_src.permute(0, 2, 1, 3) # (1|MSL, h*num_cam*w, bs, hidden)
-            # img_pe = img_pe.permute(0, 2, 1, 3) # sinusoidal skill pe
-            # img_src = img_src.flatten(0,1) # (1|MSL*num_cam*h*w, bs, hidden)
-            # img_pe = img_pe.flatten(0,1)
-            # img_src = img_src + dec_type_embed[2, :, :] # add type 3 embedding
+            # image, only use one image to decode rest of the skills
+            img_src, img_pe = img_info # (1|MSL, bs, num_cam, h*w, c&hidden)
+            img_src = self.image_proj(img_src) # (1|MSL, bs, num_cam, h*w, hidden)
+            img_src = self.image_feat_norm(img_src)
+            img_pe = img_pe * self.img_pe_scale_factor
+            img_src = img_src.flatten(2,3) # (1|MSL, bs, num_cam*h*w, hidden)
+            img_pe = img_pe.flatten(2,3)
+            img_src = img_src.permute(0, 2, 1, 3) # (1|MSL, h*num_cam*w, bs, hidden)
+            img_pe = img_pe.permute(0, 2, 1, 3) # sinusoidal skill pe
+            img_src = img_src.flatten(0,1) # (1|MSL*num_cam*h*w, bs, hidden)
+            img_pe = img_pe.flatten(0,1)
+            img_src = img_src + dec_type_embed[2, :, :] # add type 3 embedding
 
-            # dec_src = torch.cat([state_src, img_src, z_src], axis=0) # (state + img + z|MSL*(state + img + z), bs, hidden)
-            # dec_src_pe = torch.cat([state_pe, img_pe, z_pe], axis=0)
+            dec_src = torch.cat([state_src, img_src, z_src], axis=0) # (state + img + z|MSL*(state + img + z), bs, hidden)
+            dec_src_pe = torch.cat([state_pe, img_pe, z_pe], axis=0)
 
-            dec_src = torch.cat([state_src, z_src], axis=0) # (MSL*(state + z), bs, hidden)
-            dec_src_pe = torch.cat([state_pe, z_pe], axis=0)
         else:
             dec_src = z_src # (z, bs, hidden)
             dec_src_pe = z_pe
             # only get skill sections of src and mem masks with no conditional decoding
-            n_tgt = dec_tgt.shape[0]
-            src_mask = src_mask[-n_tgt:, -n_tgt:]
-            mem_mask = mem_mask[:,-n_tgt:].to(self._device)
+            src_mask = src_mask[-1:, -1:]
+            mem_mask = mem_mask[:,-1:].to(self._device)
 
         dec_src = dec_src + dec_src_pe
         dec_src = self.dec_src_norm(dec_src)